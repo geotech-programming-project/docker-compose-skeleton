c787c44952fc410bcd52d1225107388617090c89
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2024-02-13 12:57:14 +0000

    Update init.sh

--- etl/init/init.sh
+++ etl/init/init.sh
@@ -17,10 +17,10 @@ done
 echo "PostgreSQL is ready now."
 
 # Run ogr2ogr command
-ogr2ogr -f "PostgreSQL" PG:"dbname=mydb user=user password=passwd host=postgres" -nlt PROMOTE_TO_MULTI -lco GEOMETRY_NAME=wkb_geometry data/shp_sc/shp_sc.shp
+# ogr2ogr -f "PostgreSQL" PG:"dbname=mydb user=user password=passwd host=postgres" -nlt PROMOTE_TO_MULTI -lco GEOMETRY_NAME=wkb_geometry data/shp_sc/shp_sc.shp
 
-if [ $? -eq 0 ]; then
-  echo "ogr2ogr added data/shp_sc/shp_sc.shp into PostgreSQL successfully"
-else
-  echo "ogr2ogr command failed with exit code $?"
-fi
+# if [ $? -eq 0 ]; then
+#   echo "ogr2ogr added data/shp_sc/shp_sc.shp into PostgreSQL successfully"
+# else
+#   echo "ogr2ogr command failed with exit code $?"
+# fi
